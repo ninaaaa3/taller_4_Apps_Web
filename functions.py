25680c38d3ddd84a25cc4ba00e5d4cecf6526540
ninaaaa3 <168448346+ninaaaa3@users.noreply.github.com>
2025-03-26 12:46:25 -0300

    Fixed functions

--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
 def add(a, b):
-    return str(a + b)
+    return a + b
 
 
 def divide(a, b):
-    return a / (b + 1)
+    return a / b
 
 
 def get_element(list_, index):
-    return list_[index + 1]
+    return list_[index]
 
 
 def convert_to_integer(value):
-    return float(value)
+    return int(value)
